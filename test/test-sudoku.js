55cc89d76ebcfb692f0f2e60ea7b0fbb1b44f7b7
Myles Borins <myles@famo.us>
2014-10-07 05:45:16 -0700

    Add tests for Sudoku check

--- test/test-sudoku.js
+++ test/test-sudoku.js
@@ -2,6 +2,7 @@
 var test = require('tape');
 var _ = require('lodash');
 var Sudoku = require('../app/Sudoku');
+var solution = require('./fixtures/solution.json');
 
 test('Sudoku: load', function (t) {
   t.ok(Sudoku, 'it should load');
@@ -24,7 +25,13 @@ test('Sudoku: creation', function (t) {
   t.ok(_.isArray(sudoku.board), 'it should have an array called board');
   // Set a value to something the generator will never return
   sudoku.update(0, 0, 10);
+  t.equal(sudoku.board[0][0], 10, 'the board should change after being updated');
   secondHTML = sudoku.html();
   t.notEqual(firstHTML, secondHTML, 'the html returned from sudoku should be different after board has been updated');
+  
+  t.ok(typeof sudoku.check, 'function', 'it should have a method caleld check');
+  t.notok(sudoku.check(), 'check should return false if the current solution is incorrect');
+  sudoku.board = solution;
+  t.ok(sudoku.check(), 'check should return true if the solution is correct');
   t.end();
 });

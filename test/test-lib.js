5f819db3994c4f3b223176e0c76064309513bfd6
Myles Borins <myles@famo.us>
2014-10-07 04:15:27 -0700

    Update labelling for lib test

--- test/test-lib.js
+++ test/test-lib.js
@@ -4,13 +4,13 @@ var _ = require('lodash');
 
 var sudoku = require('../lib');
 
-test('sudoku-lib: loading', function (t) {
+test('lib: loading', function (t) {
   t.plan(2);
   t.ok(sudoku, 'it should load');
   t.equal(typeof sudoku, 'object', 'it should return an object');
 });
 
-test('sudoku-lib: api', function (t) {
+test('lib: api', function (t) {
   t.plan(2);
   t.ok(sudoku.generate, 'it should have a method called generate');
   t.ok(sudoku.check, 'it should have a method called check');

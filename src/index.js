01c72ae2aac84ec70a6ce43c3b581bad33f1f200
Myles Borins <myles@famo.us>
2014-10-07 01:28:25 -0700

    Solidify first pass grid design

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
+// Bootstrap styles
 require('./styles');
 
-var numberBox = require('./templates/grid.jade')({});
+var _ = require('lodash');
+var range = _.range;
+
+var grid = require('./templates/grid.jade')({
+  rows: range(9),
+  columns: range(9)
+});
 
 var app = function () {
-  document.body.innerHTML = numberBox;
+  var gridDiv = document.getElementById('grid');
+  gridDiv.innerHTML = grid;
 };
 
 window.onload = app;
